bb3f5cc4b0ba0f16b329a315f8ec1fd20527bf3d
Phimnada CHIRACHOTSUPHAPHAT <phimnada.c@ku.th>
2024-03-13 12:40:07 +0700

    Create unittest

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -7,3 +7,29 @@
 
    You can use pytest or unittest.
 """
+
+import unittest
+from counter import Counter
+
+
+class TestCounterSingleton(unittest.TestCase):
+    def test_singleton_of_the_instance(self):
+        """Checks that two instances of Counter are the same object."""
+        c1 = Counter()
+        c2 = Counter()
+        self.assertIs(c1, c2)
+
+    def test_shared_count_in_all_instances(self):
+        """Checks that the count is shared between instances."""
+        c1 = Counter()
+        c2 = Counter()
+        c1.increment()
+        self.assertEqual(c1.count, c2.count)
+
+    def test_count_not_reset_to_zero(self):
+        """Checks that the count is not reset to 0 when a new instance is created after incrementing the count."""
+        c1 = Counter()
+        c1.increment()
+        count_before_reset = c1.count
+        c2 = Counter()
+        self.assertEqual(count_before_reset, c2.count)
